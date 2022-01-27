22a22167f431ba5c1bb9a6bc236d9c68fb16c132
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2022-01-27 09:19:06 -0800

    fixing spelling

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
+
+x_values = range(1,1001)
+y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 fig,ax =plt.subplots()
-ax.scatter(2,4,s=200)
+ax.scatter(x_values,y_values,s=10)
 
 #set chart title and labels axes.
 ax.set_title("Square Numbers",fontsize=24)
@@ -10,4 +13,7 @@ ax.set_ylabel("Square of Value",fontsize =14)
 
 #set size of tick labels
 ax.tick_params(axis='both',which='major',labelsize=14)
+
+#set the range for each axis.
+ax.axis([0,1100,0,1100000])
 plt.show()
